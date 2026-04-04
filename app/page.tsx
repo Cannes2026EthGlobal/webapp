ee314b43f43e7b3579d524a9cb5ad1befc9ccb10
in-th3-l00p <admin@tiscacatalin.com>
2026-04-04 12:47:38 +0200

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import { Demo } from "@/components/demo"
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
-  return <Demo />
+  return (
+    <section className="w-screen h-screen flex items-center justify-center">
+      <div className="text-center max-w-xl">
+        <h1 className="text-2xl font-semibold">arc-counting</h1>
+        <p>SaaS accounting platform for private payment, payroll, invoicing, and real-time usage settlement </p>
+
+        <div className="mt-4 flex justify-center items-center">
+          <Button>login</Button>
+        </div>
+      </div>
+    </section>
+  );
 }
